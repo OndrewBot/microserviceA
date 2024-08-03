9a5ed2341e525121f6d4c18a76e959a38fc36122
Andrew Bottom <bottoma@oregonstate.edu>
2024-08-03 09:53:54 -0400

    cups to tbs

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+known_units = set("cups", "tbs", "tsp")
+
+@app.get("/convert/units:{units}+amount:{amount}+target-units:{target}")
+def read_item(units: str, amount: int, target: str):
+    if (units not in known_units)or (target not in known_units) :
+        return {"invalid units - must be 'cups', 'tbs', 'tsp'"}
+    if units == target:
+        return {"units": units, "amount": amount}
+    if units == "cups":
+        if target == "tbs":
+            amount = amount * 16
+        elif target == "tsp":
+            pass
+    elif units == "tbs":
+        pass
+    elif units == "tsp":
+        pass
+    
+    return {"units": units, "amount": amount}
